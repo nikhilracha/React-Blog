74a3d8f503d946201a45c5bf579dd362f40d0319
Nikhil Racha <rachanikhil41@gmail.com>
2020-05-07 12:46:52 -0400

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -61,57 +61,7 @@ const Main = (props) => {
             "source": "https://www.medicalnewstoday.com/articles/322868"
         }
     ]
-    // const featuredPosts = [
-    //     {
-    //         title: 'Featured post',
-    //         date: 'Nov 12',
-    //         description:
-    //             'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    //         image: 'https://source.unsplash.com/random',
-    //         imageText: 'Image Text',
-    //     },
-    //     {
-    //         title: 'Post title',
-    //         date: 'Nov 11',
-    //         description:
-    //             'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    //         image: 'https://source.unsplash.com/random',
-    //         imageText: 'Image Text',
-    //     },
-    //     {
-    //         title: 'Post title',
-    //         date: 'Nov 11',
-    //         description:
-    //             'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    //         image: 'https://source.unsplash.com/random',
-    //         imageText: 'Image Text',
-    //     },
-    //     {
-    //         title: 'Post title',
-    //         date: 'Nov 11',
-    //         description:
-    //             'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    //         image: 'https://source.unsplash.com/random',
-    //         imageText: 'Image Text',
-    //     },
-    //     {
-    //         title: 'Post title',
-    //         date: 'Nov 11',
-    //         description:
-    //             'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    //         image: 'https://source.unsplash.com/random',
-    //         imageText: 'Image Text',
-    //     },
-    //     {
-    //         title: 'Post title',
-    //         date: 'Nov 11',
-    //         description:
-    //             'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    //         image: 'https://source.unsplash.com/random',
-    //         imageText: 'Image Text',
-    //     },
-
-    // ];
+    
     const classes = useStyles();
 
     return (
